d5208d0f6f7bd2383c3074af0668b722049139d9
Travis Pandos <travis.pandos@gamaaviation.com>
2021-08-12 09:59:38 -0700

    initial commit, add pathfinder, node components

---
+++ src/components/Pathfinder.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pathfinder = () => {
+    return (
+        <div>
+            <h1>Pathfinder Component</h1>
+        </div>
+    )
+}
+
+export default Pathfinder
